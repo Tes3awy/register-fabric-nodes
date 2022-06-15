debca0f0bb3b64e13e24780d56eedb6c74e2e2b4
tes3awy <osaid2512@gmail.com>
2022-06-15 20:27:55 +0200

    Add try/exception blocks

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from getpass import getpass
 from time import perf_counter
 from warnings import filterwarnings
 
+from requests.exceptions import HTTPError, InvalidURL, Timeout
 from rich import print
 
 from apic_login import apic_login
@@ -16,16 +17,19 @@ filterwarnings(action="ignore", message=r"Unverified\sHTTPS\srequest\s.*")
 def main():
     # Inputs
     nodes_file = input("Fabric Nodes Excel file: ").strip() or "Fabric-Nodes.xlsx"
-    apic = input("APIC IP Address: ").strip() or "198.18.133.200"
+    apic = input("APIC IP Address: ").strip() or "sandboxapicdc.cisco.com"
     usr = input("Username: ").strip() or "admin"
-    pwd = getpass(prompt="Password: ").strip() or "C1sco12345"
+    pwd = getpass(prompt="Password: ").strip() or "!v3G@!4@Y"
 
     ## Processing
     # Login to APIC and get tokens
-    print("Logging into APIC...", end="\r")
-    res = apic_login(apic=apic, usr=usr, pwd=pwd)
-    if not isinstance(res, str):
-        print("[green]Logged into APIC successfully")
+    print(f"Logging into {apic}...", end="\r")
+    try:
+        res = apic_login(apic=apic, usr=usr, pwd=pwd)
+    except (InvalidURL, HTTPError, ConnectionError, Timeout) as e:
+        raise SystemExit(print(f"[red]{e}")) from e
+    else:
+        print(f"[green]Logged into {apic} successfully")
 
         headers = {
             "cookie": res.headers.get("set-cookie"),
@@ -36,24 +40,31 @@ def main():
             .get("urlToken"),
         }
 
-        # Read nodes from Excel file
-        nodes = read_nodes(nodes_file)
+        try:
+            # Read nodes from Excel file
+            nodes = read_nodes(nodes_file)
+        except FileNotFoundError as e:
+            raise SystemExit(
+                print(f"[red]{nodes_file} is not found! Check typos in filename")
+            ) from e
+        else:
+            start_time = perf_counter()
 
-        start_time = perf_counter()
+            # Register nodes to ACI Fabric
+            for n, node in enumerate(iterable=nodes, start=1):
+                print(f"{n}/{len(nodes)} Registering node {node['name']}...", end="\r")
+                reg = register_node(apic=apic, headers=headers, node=node)
+                print(reg)
 
-        # Register all nodes to APIC
-        for n, node in enumerate(iterable=nodes, start=1):
-            print(f"{n}/{len(nodes)} Registering node {node['name']}...", end="\r")
-            reg = register_node(apic=apic, headers=headers, node=node)
-            print(reg)
-
-        # Output
-        print(f"[green]Registered {n}/{len(nodes)} APIC nodes", end="\n\n")
-        print(f"EET: {perf_counter() - start_time:.2f} seconds")
-
-        # Logout of APIC
-        status = apic_logout(apic=apic, headers=headers, usr=usr)
-        print(status)
+            # Output
+            print(f"[green]Registered {n}/{len(nodes)} APIC nodes", end="\n\n")
+            print(f"EET: {perf_counter() - start_time:.2f} seconds")
+        finally:
+            if res is not None:
+                # Logout from APIC
+                status = apic_logout(apic=apic, headers=headers, usr=usr)
+                if "deleted" in status.headers.get("set-cookie"):
+                    print("[yellow]Logged out")
 
 
 if __name__ == "__main__":
