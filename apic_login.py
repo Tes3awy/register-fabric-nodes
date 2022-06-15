547c161da3e1c9977224e34d37ad511f8a88b4ac
tes3awy <osaid2512@gmail.com>
2022-06-15 20:28:16 +0200

    Remove try/exception blocks

--- apic_login.py
+++ apic_login.py
@@ -1,15 +1,10 @@
 import requests
 from requests import Response
-from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
 
 
 def apic_login(apic: str, usr: str, pwd: str) -> Response:
     url = f"https://{apic}/api/aaaLogin.json?gui-token-request=yes"
     payload = {"aaaUser": {"attributes": {"name": usr, "pwd": pwd}}}
-    try:
-        r = requests.post(url=url, json=payload, timeout=8.0, verify=False)
-        r.raise_for_status()
-    except (InvalidURL, HTTPError, ConnectionError, Timeout) as e:
-        return f"[red]{e}"
-    else:
-        return r
+    r = requests.post(url=url, json=payload, timeout=10.0, verify=False)
+    r.raise_for_status()
+    return r
