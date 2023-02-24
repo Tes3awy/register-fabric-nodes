9cfabf95ddb7ec2063608771148298169b92a26b
tes3awy <11368432+Tes3awy@users.noreply.github.com>
2023-02-24 14:57:01 +0200

    Refactor code

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 # -*- coding: utf-8 -*-
 from getpass import getpass
 from warnings import filterwarnings
-from xml.etree import ElementTree as ET
 
-from requests.exceptions import HTTPError, InvalidURL, Timeout
+from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
 from rich import print
 
-import apic
+import auth
 import nodes
 
 filterwarnings(action="ignore", message=r"Unverified\sHTTPS\srequest\s.*")
 
 
+def get_input(prompt: str, default: str = None):
+    """Helper function to get user input with an optional default value"""
+    value = input(prompt).strip()
+    return default if value == "" else value
+
+
 def main():
     # Inputs
-    nodes_file = input("Fabric Nodes Excel file: ").strip() or "Fabric-Nodes.xlsx"
-    apic_ip = input("APIC IP Address: ").strip() or "sandboxapicdc.cisco.com"
-    usr = input("Username: ").strip() or "admin"
-    pwd = getpass(prompt="Password: ").strip() or "!v3G@!4@Y"
+    nodes_file = get_input("Nodes Excel file: ", default="Fabric-Nodes.xlsx")
+    apic_ip = get_input("APIC IP Address: ", default="sandboxapicdc.cisco.com")
+    usr = get_input("Username: ", default="admin")
+    pwd = getpass(prompt="Password: ") or "!v3G@!4@Y"
 
     ## Processing
-    # Login to APIC to create a session and get tokens
-    print(f"Logging into {apic_ip}...", end="\r")
+    # Accessing APIC
+    print(f"\nAccessing {apic_ip}...", end="\r")
     try:
-        res = apic.login(apic=apic_ip, usr=usr, pwd=pwd)
+        r = auth.login(apic=apic_ip, usr=usr, pwd=pwd)
     except (InvalidURL, HTTPError, ConnectionError, Timeout) as e:
-        raise SystemExit(print(f"[red]{e}")) from e
+        raise SystemExit(e) from e
     else:
-        print(f"[green]Logged into {apic_ip} successfully")
+        print(f"[green]Accessed {apic_ip} successfully")
 
         headers = {
-            "cookie": res.headers.get("set-cookie"),
-            "apic-challenge": res.json()
+            "cookie": r.headers.get("set-cookie"),
+            "apic-challenge": r.json()
             .get("imdata")[0]
             .get("aaaLogin")
             .get("attributes")
@@ -42,36 +47,34 @@ def main():
             # Read nodes from Excel file
             fab_nodes = nodes.read(nodes_file)
         except FileNotFoundError as e:
-            raise SystemExit(
-                print(f"[red]{nodes_file} is not found! Check typos in filename")
-            ) from e
+            raise SystemExit(f"{nodes_file} is not found! Check typos") from e
         else:
             # Register nodes to ACI Fabric
             i, eet = 0, 0.0
             for node in fab_nodes:
-                print(f"Registering node {node.get('name')}...", end="\r")
+                print(f"Registering {node.get('name')}...", end="\r")
                 try:
-                    reg_res = nodes.register(apic=apic_ip, headers=headers, node=node)
-                    eet += reg_res.elapsed.total_seconds()
+                    reg = nodes.register(apic=apic_ip, headers=headers, node=node)
+                    eet += reg.elapsed.total_seconds()
                 except HTTPError as e:
                     print(
-                        f"[red]{node.get('name')}: {ET.fromstring(text=e.response.text).find(path='.//error').get(key='text')}"
+                        f"[red]{node.get('name')}: {e.response.json().get('imdata')[0].get('error').get('attributes').get('text')}"
                     )
                 else:
                     i += 1
                     print(
-                        f"[blue]Registered {node.get('name')} (ID: {node.get('node_id')}) with serial number {node.get('serial')} to APIC"
+                        f"[green]Registered {node.get('name')} with ID {node.get('node_id')} and S/N {node.get('serial')}"
                     )
 
             # Output
-            print(f"[green]Registered {i}/{len(fab_nodes)} APIC nodes", end="\n\n")
-            print(f"EET: {eet:.2f} seconds")
-        finally:
-            if res is not None:
-                # Logout from APIC
-                out_res = apic.logout(apic=apic_ip, headers=headers, usr=usr)
-                if "deleted" in out_res.headers.get("set-cookie") and out_res.ok:
-                    print(f"[magenta]Logged out from {apic_ip}")
+            print(
+                f"[blue]Registered {i}/{len(fab_nodes)} nodes in {eet:.2f} seconds",
+                end="\n\n",
+            )
+
+        out_res = auth.logout(apic=apic_ip, headers=headers, usr=usr)
+        if out_res.ok and "deleted" in out_res.headers.get("set-cookie"):
+            print(f"[magenta]Closed {apic_ip} session")
 
 
 if __name__ == "__main__":
