79ae2b4c4f6530d1c2656ba4ecf79454198c1605
tes3awy <11368432+Tes3awy@users.noreply.github.com>
2023-02-24 14:57:36 +0200

    Refactor code

--- apic_cobra.py
+++ apic_cobra.py
@@ -14,53 +14,52 @@ from rich import print
 import nodes
 
 
-def register_fabric_nodes():
+def register_fabric_nodes():  # sourcery skip: extract-method
     # Suppress HTTPS warning
     warnings.filterwarnings(action="ignore", message=r"Unverified\sHTTPS\srequest\s.*")
 
     ## Inputs
-    nodes_file = input("Fabric Nodes Excel file: ").strip() or "Fabric-Nodes.xlsx"
+    nodes_file = input("Nodes Excel file: ").strip() or "Fabric-Nodes.xlsx"
     apic = input("APIC IP Address: ").strip() or "sandboxapicdc.cisco.com"
-    username = input("Username: ").strip() or "admin"
-    password = getpass(prompt="Password: ").strip() or "!v3G@!4@Y"
+    usr = input("Username: ").strip() or "admin"
+    pwd = getpass(prompt="Password: ").strip() or "!v3G@!4@Y"
 
     ## Processing
     apic_session = session.LoginSession(
         controllerUrl=f"https://{apic}",
-        user=username,
-        password=password,
+        user=usr,
+        password=pwd,
         secure=False,
-        timeout=10,  # optional (default 90 seconds)
-        requestFormat="json",  # optional but recommended (default xml)
+        requestFormat="json",
     )
     moDir = access.MoDirectory(session=apic_session)
 
     try:
-        print("\nLogging in into APIC...", end="\r")
+        print(f"\nAccessing {apic_session.controllerUrl}...", end="\r")
         moDir.login()
     except (HTTPError, ConnectionError, InvalidURL) as e:
-        raise SystemExit(print(f"[red]{e}")) from e
+        raise SystemExit(e) from e
     else:
-        print(f"[green]Logged into APIC as {apic_session.user}")
+        print(f"[green]Successfully accessed APIC as {apic_session.user}")
 
         # select the top level object
         polUni = pol.Uni(parentMoOrDn="")
+        ctrlrInst = ctrlr.Inst(parentMoOrDn=polUni)
 
         fabric_nodes = nodes.read(nodes_file)  # read ACI Nodes from Excel file
 
         start_time = time.perf_counter()
         # register fabric nodes
         for node in fabric_nodes:
-            ctrlrInst = ctrlr.Inst(parentMoOrDn=polUni)
             fabricNodeIdentPol = fabric.NodeIdentPol(parentMoOrDn=ctrlrInst)
             fabricNodeIdentP = fabric.NodeIdentP(
                 parentMoOrDn=fabricNodeIdentPol,
-                name=node["name"].strip(),
-                serial=node["serial"].strip(),
-                podId=node["pod_id"] or "1",
-                nodeId=node["node_id"],
-                nodeType=node["type"].lower().strip(),
-                role=node["role"].lower().strip(),
+                name=node.get("name").strip(),
+                serial=node.get("serial").strip(),
+                podId=node.get("pod_id") or "1",
+                nodeId=node.get("node_id"),
+                nodeType=node.get("type").lower().strip(),
+                role=node.get("role").lower().strip(),
             )
 
             ## Output
@@ -74,16 +73,15 @@ def register_fabric_nodes():
                 response = moDir.commit(configObject=cfg_request)
             except (CommitError, RestError) as e:
                 print(
-                    f"[red]{json.loads(e.reason)['imdata'][0]['error']['attributes']['text']}"
+                    f"[red]{json.loads(e.reason).get('imdata')[0].get('error').get('attributes').get('text')}"
                 )
             else:
-                if response.status_code in (200, 201) and response.ok:
+                if response.status_code in (200, 201):
                     print(
-                        f"[magenta]Registered {fabricNodeIdentP.name} (ID {fabricNodeIdentP.nodeId}) with serial number {fabricNodeIdentP.serial} to APIC"
+                        f"[magenta]Registered {fabricNodeIdentP.name} with ID {fabricNodeIdentP.nodeId} and serial number {fabricNodeIdentP.serial}"
                     )
         print(f"EET: {time.perf_counter() - start_time:.2f} second")
-    finally:
-        # check if logged in already to logout
+
         if moDir.session.cookie:
             moDir.logout()  # logout
             print("[yellow]Logged out")
